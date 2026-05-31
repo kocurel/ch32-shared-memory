14f3d58d95a849be9a8798545acfa7348a3af696
kocurel <67975387+kocurel@users.noreply.github.com when>
2026-05-31 15:01:39 +0200

    feat(hw): implement bare-metal gpio handshake configuration

--- firmware_ch32/main.c
+++ firmware_ch32/main.c
@@ -3,11 +3,39 @@
 //    Copyright (C) 2026 Emilia Koziol
 
 #include "ch32fun.h"
+#include "ch32v003hw.h"
+
+#define BUSREQ_PIN 1
+#define BUS_GRANT_PIN 2
+
+#define GPIO_CFG_CLR_MASK(pin)      (0xF << ((pin) * 4))
+#define GPIO_CFG_SET_MASK(pin, cfg) ((cfg )<< ((pin) * 4))
+
+#define M1
+// #define M2
 
 int main()
 {
     SystemInit();
+    funGpioInitAll();
 
+    RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
+
+    GPIOC->CFGLR &= ~(GPIO_CFG_CLR_MASK(BUSREQ_PIN) | GPIO_CFG_CLR_MASK(BUS_GRANT_PIN));
+
+#ifdef M1
+
+    GPIOC->CFGLR |= GPIO_CFG_SET_MASK(BUSREQ_PIN, GPIO_CFGLR_IN_FLOAT);
+    GPIOC->CFGLR |= GPIO_CFG_SET_MASK(BUS_GRANT_PIN, GPIO_CFGLR_OUT_10Mhz_OD);
+
+#endif
+
+#ifdef M2
+
+    GPIOC->CFGLR |= GPIO_CFG_SET_MASK(BUSREQ_PIN, GPIO_CFGLR_OUT_10Mhz_OD);
+    GPIOC->CFGLR |= GPIO_CFG_SET_MASK(BUS_GRANT_PIN, GPIO_CFGLR_IN_FLOAT);
+
+#endif
     while (1) {
     }
 }
